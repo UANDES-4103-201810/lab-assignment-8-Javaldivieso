f85141a4495a45bf5b47b82571e79516db55e97c
Javaldivieso <javaldivieso1@miaundes.cl>
2018-05-08 14:22:43 -0300

    lab

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :products
+  resources :users do 
+	resources :Products,  controller:'users/products'
+  end
+  get 'admin_home', to: 'users#admin_home'
+  get 'admin_users', to: 'users#admin_users'
+  get 'admin_products', to: 'users#admin_products'
+  get 'admin_users_products', to: 'users#admin_users_products'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
